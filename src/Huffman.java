6239df1b92160f9163ebf85e89ef4e3b0f2f007a
nathan-beneke <benek020@morris.umn.edu>
2017-12-05 13:10:52 -0600

    Create Huffman from 1st line of input Decode, and encode lines from input Error handling.

--- src/Huffman.java
+++ src/Huffman.java
@@ -12,7 +12,7 @@ public class Huffman {
         for(CharFreqPair charFreq : charFreqs){
             queue.add(new Node(charFreq.freq, charFreq.chr));
         }
-        
+
         // Collapse the queue until there is only the root of the tree left
         while(queue.size() > 1){
             Node first = queue.poll();
@@ -25,47 +25,93 @@ public class Huffman {
                 queue.add(new Node(total, second, first));
             }
         }
-        
-        this.root = queue.poll();
-        makeCodes("",root);
 
+        if (charFreqs.size() == 1){
+            Node alone = new Node ((byte) 100, queue.poll().chr);
+            this.root = new Node((byte) 100, null, alone);
+        } else {
+            this.root = queue.poll();
+        }
+
+        makeCodes("", root);
+        System.out.println();
 
     }
 
     // Recursively travel Huffman tree to find the encodings
     public void makeCodes(String codeSoFar, Node node){
-        if(node.leaf){
-            charCodes.add(new Code(node.chr, codeSoFar));
+        if (node == null){
+            return;
+        } else if(node.leaf){
+            Code code = new Code(node.chr, codeSoFar);
+            charCodes.add(code);
+            System.out.print(code);
         } else {
             makeCodes(codeSoFar + "1", node.one);
             makeCodes(codeSoFar + "0", node.zero);
         }
-        //TODO I think this will work correctly
     }
 
     public String encodeString(String toEnc){
-        String code = "";
+        try {
+            String code = "";
 
-        for (char chr : toEnc.toCharArray()){
-            code = code + encodeChr(chr);
+            for (char chr : toEnc.toCharArray()) {
+                code = code + encodeChr(chr);
+            }
+
+            return code;
+        } catch (UnsupportedOperationException e){
+            return "Unsupported character used";
         }
-
-        return code;
     }
 
     //Uses arraylist of codes to find the code for this character
-    public String encodeChr(char chr){
+    private String encodeChr(char chr) throws UnsupportedOperationException{
         String enCode = "";
 
         for (int i = 0; i < charCodes.size(); i++){
             Code code = charCodes.get(i);
             if (code.chr == chr){
-                enCode = code.code;
-                break;
+                return code.code;
             }
         }
 
-        return enCode;
+        throw new UnsupportedOperationException();
+    }
+
+    // Traverses the code tree to decode the String
+    public String decodeString(String code){
+        String str = "";
+        Node current = root;
+
+        for (int i = 0; i < code.length(); i++){
+            // If the node is a leaf, we've found the next character
+            if (current == null){
+                return "Invalid encoding";
+            }
+            if (current.leaf){
+                str = str + current.chr;
+                current = root;
+            }
+
+            // Traverse the tree until we find the right character
+            if (code.charAt(i) == '0'){
+                current = current.zero;
+            } else if (code.charAt(i) == '1'){
+                current = current.one;
+            } else {
+                return "Invalid encoding";
+            }
+        }
+
+        if(current.leaf) {
+            str = str + current.chr;
+        } else {
+            return "Invalid encoding";
+        }
+
+        return str;
     }
 
     public class Node implements Comparable<Node>{
@@ -88,7 +134,7 @@ public class Huffman {
         }
 
         public int compareTo(Node node){
-            return node.freq - this.freq;
+            return this.freq - node.freq;
         }
     }
 
@@ -99,6 +145,10 @@ public class Huffman {
         public Code(char chr, String code){
             this.chr = chr;
             this.code = code;
+        }
+
+        public String toString(){
+            return chr + ": " + code + "  ";
         }
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+        System.out.println("Enter your letters and frequencies (e.g. a 51 b 22 c 15 d 7 e 5):");
         Scanner scan = new Scanner(System.in);
         String line = scan.nextLine();
+
+        Huffman huff = makeHuffman(line, scan);
 
 
         while(scan.hasNextLine()){
@@ -14,7 +18,53 @@ public class Main {
             if (line.equals("x")){
                 return;
             }
+            // if the first character is an e, encode the next word.
+            else if (line.charAt(0) == 'e'){
+                String toDecode = line.split(" ")[1];
+                System.out.println(huff.encodeString(toDecode));
+            }
+            // if the first character is an d, decode the next word.
+            else if (line.charAt(0) == 'd'){
+                String toDecode = line.split(" ")[1];
+                System.out.println(huff.decodeString(toDecode));
+            } else {
+                System.out.println("what?");
+            }
 
         }
+    }
+
+    // Parses the line given into CharFreqPairs and makes a Huffman with them.
+    // If invalid data is given, prompts you to try again
+    public static Huffman makeHuffman(String line, Scanner scan){
+        String[] arr = line.split(" ");
+        ArrayList<CharFreqPair> pairs = new ArrayList<CharFreqPair>();
+        byte total = 0;
+
+        for (int i = 0; i < arr.length; i+=2){
+            byte freq;
+
+            try {
+                freq = Byte.parseByte(arr[i + 1]);
+            } catch (Exception e){
+                System.out.println("Invalid format. Try again");
+                return makeHuffman(scan.nextLine(), scan);
+            }
+            total += freq;
+
+            if (arr[i].length() != 1){
+                System.out.println("Invalid character used. Try again");
+                return makeHuffman(scan.nextLine(), scan);
+            }
+            CharFreqPair pair = new CharFreqPair(arr[i].charAt(0), freq);
+            pairs.add(pair);
+        }
+
+        if (total > 101 || total < 99){
+            System.out.println("Invalid total frequency of letters, try again");
+            return makeHuffman(scan.nextLine(), scan);
+        }
+
+        return new Huffman(pairs);
     }
 }
